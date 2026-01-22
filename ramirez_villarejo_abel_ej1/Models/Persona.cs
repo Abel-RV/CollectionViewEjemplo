0339b5a19c130991913a970f178ecc73c01c3401
Abel-RV <ramirezvillarejoabel@gmail.com>
2026-01-22 10:25:25 +0100

    Commit final finaloso de los mas finales

--- ramirez_villarejo_abel_ej1/Models/Persona.cs
+++ ramirez_villarejo_abel_ej1/Models/Persona.cs
@@ -1,17 +1,66 @@
-﻿
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
 namespace CollectionViewEjemplo.Models
 {
-    internal class Persona
+    // Implementamos la interfaz para notificar cambios
+    internal class Persona : INotifyPropertyChanged
     {
-        public string PersonaName {  get; set; }
-        public string PersonaApellidos { get; set; }
-        public string PersonaFoto { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
 
-        public string FechaNacimiento { get; set; }
+        private string _personaName;
+        private string _personaApellidos;
+        private string _trabajo;
+        private string _direccion;
+        private string _telefono;
+        private string _fechaNacimiento;
+        private string _personaFoto;
 
-        public string Direccion { get; set; }
-        public string Trabajo { get; set; }
-        public string Telefono { get; set; }
-    
+        public string PersonaName
+        {
+            get => _personaName;
+            set { if (_personaName != value) { _personaName = value; OnPropertyChanged(); } }
+        }
+
+        public string PersonaApellidos
+        {
+            get => _personaApellidos;
+            set { if (_personaApellidos != value) { _personaApellidos = value; OnPropertyChanged(); } }
+        }
+
+        public string Trabajo
+        {
+            get => _trabajo;
+            set { if (_trabajo != value) { _trabajo = value; OnPropertyChanged(); } }
+        }
+
+        public string Direccion
+        {
+            get => _direccion;
+            set { if (_direccion != value) { _direccion = value; OnPropertyChanged(); } }
+        }
+
+        public string Telefono
+        {
+            get => _telefono;
+            set { if (_telefono != value) { _telefono = value; OnPropertyChanged(); } }
+        }
+        
+        public string FechaNacimiento
+        {
+            get => _fechaNacimiento;
+            set { if (_fechaNacimiento != value) { _fechaNacimiento = value; OnPropertyChanged(); } }
+        }
+
+        public string PersonaFoto
+        {
+            get => _personaFoto;
+            set { if (_personaFoto != value) { _personaFoto = value; OnPropertyChanged(); } }
+        }
     }
 }
--- ramirez_villarejo_abel_ej1/Pages/DetallePersonaPage.cs
+++ ramirez_villarejo_abel_ej1/Pages/DetallePersonaPage.cs
@@ -4,9 +4,36 @@ namespace CollectionViewEjemplo.Pages;
 
 public partial class DetallePersonaPage : ContentPage
 {
+    private bool esModoEdicion = false;
+
     internal DetallePersonaPage(Persona persona)
     {
         InitializeComponent();
         BindingContext = persona;
+    }
+
+    private void OnEditarClicked(object sender, EventArgs e)
+    {
+        // Invertimos el estado de edición
+        esModoEdicion = !esModoEdicion;
+        txtNombre.IsReadOnly = !esModoEdicion;
+        txtApellidos.IsReadOnly = !esModoEdicion;
+        txtTrabajo.IsReadOnly = !esModoEdicion;
+        txtDireccion.IsReadOnly = !esModoEdicion;
+        txtFecha.IsReadOnly = !esModoEdicion;
+
+        if (esModoEdicion)
+        {
+            btnEditar.Text = "Guardar Cambios";
+            btnEditar.BackgroundColor = Colors.Orange;
+            
+            txtNombre.Focus();
+        }
+        else
+        {
+            btnEditar.Text = "Editar Datos";
+            btnEditar.BackgroundColor = Color.FromArgb("#512BD4");
+            DisplayAlert("Guardado", "Los datos han sido actualizados", "OK");
+        }
     }
 }
